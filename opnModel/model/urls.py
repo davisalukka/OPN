850793fdb6ab463dd9111f90a8420b3db763c0df
Davis Alukka <ajdavis19@gmail.com>
2018-12-01 21:25:51 -0500

    Django initial set up

---
+++ opnModel/model/urls.py
@@ -0,0 +1,8 @@
+#model/urls.py
+from django.conf.urls import url
+from model import views
+
+urlpatterns = [
+        url(r'^$', views.HomePageView.as_view()),
+        url(r'^about/$', views.AboutPageView.as_view()), #Add this /about/ route
+        ]
