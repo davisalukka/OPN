06d89be090cacd8d4d8aa9eb9737d35ed09cd590
Davis Alukka <ajdavis19@gmail.com>
2019-04-12 15:01:25 -0500

    Included confidenceLevel.py, calcualtes confidence of valuation

---
+++ confidenceLevel.py
@@ -0,0 +1,43 @@
+import MySQLdb
+
+db = MySQLdb.connect("localhost", "root", "fIf@Ty5050!", "opnalpha")
+
+cur = db.cursor()
+
+print("Company manifest: \n")
+
+cur.execute("SELECT company_name FROM decision_metrics")
+counter = 1
+for row in cur.fetchall():
+	print "",counter," ",row[0],""
+	counter = counter + 1
+
+endpoint = raw_input("Enter Company name:")
+print("\n")
+print("Monthly earnings report:")
+query1 = """SELECT * FROM monthly_earnings WHERE company_name = %s"""
+cur.execute(query1, (endpoint, ))
+record = cur.fetchall()
+for i in range(len(cur.description)):
+	for row in record:
+		print (str(cur.description[i][0])+": "+str(row[i]))
+
+print("\n")
+print("Annual earnings report:")
+total_earnings = 0
+current_month = 0
+confidence = 0
+
+
+for j in range(1, len(cur.description)):
+	for row in record:
+		if(row[j] >= current_month):
+			confidence += 1	
+		total_earnings += row[j]
+		current_month = row[j]
+
+print("Annual earnings: "+str(total_earnings))
+confidence = (confidence / float(12)) * 100
+print("confidence: "+str(confidence))
+
+db.close()
